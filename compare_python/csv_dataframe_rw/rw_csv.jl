6a2cf5bec14f7243587674237387b10866906917
wanggangshan <wanggangshan@jd.com>
2019-04-16 18:44:14 +0800

    fix jl

--- compare_python/csv_dataframe_rw/rw_csv.jl
+++ compare_python/csv_dataframe_rw/rw_csv.jl
@@ -1,7 +1,22 @@
 
+using Dates
+using CSV
+using DataFrames
 
-Using CSV
-Using DataFrames
+path_log = joinpath(dirname(@__FILE__), "_DATA_USE")
+open(path_log, "r") do io
+    global file_path = strip(read(io, String))
+    file_path = joinpath(dirname(@__FILE__), file_path)
+end
 
-df = CSV.read("./data/test.csv")
-println(df)
+write_path = joinpath(dirname(@__FILE__), "test_w.csv")
+
+
+println("filepath: $file_path\n")
+println("start read csv =========")
+@time df = CSV.read(file_path)
+
+println("\nstart write csv =========")
+
+@time CSV.write(write_path, df)
+rm(write_path)
